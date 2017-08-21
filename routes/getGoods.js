96dad5983ccf90605b3c73b5a6e44539540632b2
孙昊翔 <673686754@qq.com>
2017-08-21 21:06:39 +0800

    goods接口修改

--- routes/getGoods.js
+++ routes/getGoods.js
@@ -4,42 +4,45 @@ const getGoods = require('../models/getGoods')
 
 // 获取相应分类数据
 router.get('/:productClass', (req, res, next) => {
-  if (req.params.productClass === 'hot') {
-    getGoods.find({hot: true}, (err, doc) => {
-      if (err) {
-        res.json({
-          status: '1',
-          msg: err.message
-        })
-      } else {
-        res.json({
-          status: '0',
-          msg: '',
-          result: {
-            count: doc.length,
-            list: doc
-          }
-        })
-      }
-    })
-  } else {
-    getGoods.find({productClass: req.params.productClass}, (err, doc) => {
-      if (err) {
-        res.json({
-          status: '1',
-          msg: err.message
-        })
-      } else {
-        res.json({
-          status: '0',
-          msg: '',
-          result: {
-            count: doc.length,
-            list: doc
-          }
-        })
-      }
-    })
+  switch (req.params.productClass) {
+    case 'hot':
+      getGoods.find({hot: true}, (err, doc) => {
+        if (err) {
+          res.json({
+            status: '1',
+            msg: err.message
+          })
+        } else {
+          res.json({
+            status: '0',
+            msg: '',
+            result: {
+              count: doc.length,
+              list: doc
+            }
+          })
+        }
+      })
+      break;
+    default:
+      getGoods.find({productClass: req.params.productClass}, (err, doc) => {
+        if (err) {
+          res.json({
+            status: '1',
+            msg: err.message
+          })
+        } else {
+          res.json({
+            status: '0',
+            msg: '',
+            result: {
+              count: doc.length,
+              list: doc
+            }
+          })
+        }
+      })
+      break;
   }
 })
 
