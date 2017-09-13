fa1ddd1a4f690304d162fa333a4500815b47c268
孙昊翔 <673686754@qq.com>
2017-09-13 15:54:10 +0800

    上传指定文件夹修改

--- routes/users/upload.js
+++ routes/users/upload.js
@@ -3,7 +3,7 @@ const path = require('path')
 const express = require('express')
 const router = express.Router()
 const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ dest: 'upload/' })
 
 module.exports = function () {
   // 上传
