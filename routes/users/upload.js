7cc98a608748e0ebd9340d408e6409139119c96c
孙昊翔 <673686754@qq.com>
2017-09-15 10:32:48 +0800

    upload接口添加token验证

--- routes/users/upload.js
+++ routes/users/upload.js
@@ -2,26 +2,48 @@ const fs =require('fs')
 const path = require('path')
 const express = require('express')
 const router = express.Router()
+const jwt = require('../../jwt/jwt')
 const multer = require('multer')
-const upload = multer({ dest: 'upload/' })
+const upload = multer({
+  dest: 'upload/',
+  limits: {
+    fileSize: 2 * 1024 * 1024,
+  },
+  fileFilter: (req, file, cb) => {
+    let verifyToken = jwt.verify(req.body.token)
+    if (verifyToken === 'invalid') {
+      cb(null, false)
+      cb(new Error())
+    } else {
+      cb(null, true)
+    }
+
+  }
+}).single('image')
 
 module.exports = function () {
   // 上传
-  router.post('/', upload.single('image'), (req, res, next) => {
-    let newPath = req.file.path + path.parse(req.file.originalname).ext
-    // 为上传的文件加上扩展名
-    fs.rename(req.file.path, newPath, (err) => {
+  router.post('/', (req, res, next) => {
+    upload(req, res, (err) => {
       if (err) {
-        res.json({
-          status: -1,
-          msg: err.message
-        })
+        console.log('Upload error.')
       } else {
-        res.json({
-          status: 1,
-          msg: '',
-          result: {
-            path: newPath
+        let newPath = req.file.path + path.parse(req.file.originalname).ext
+        // 为上传的文件加上扩展名
+        fs.rename(req.file.path, newPath, (err) => {
+          if (err) {
+            res.json({
+              status: -1,
+              msg: err.message
+            })
+          } else {
+            res.json({
+              status: 1,
+              msg: '',
+              result: {
+                path: newPath
+              }
+            })
           }
         })
       }
