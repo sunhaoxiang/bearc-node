bbd65e562168ac655752ee651bf43354b92ba984
孙昊翔 <673686754@qq.com>
2018-01-17 15:59:17 +0800

    修改商品接口开发

--- routes/users/goodsList.js
+++ routes/users/goodsList.js
@@ -14,7 +14,7 @@ module.exports = function () {
       })
     } else {
       goods().find({}, {
-        _id: 0,
+        _id: 1,
         productName: 1,
         purchasePrice: 1,
         productPrice: 1,
@@ -34,7 +34,7 @@ module.exports = function () {
           if (remainTime > 900) {
             res.json({
               status: 0,
-              msg: '验证成功',
+              msg: '查询成功',
               result: {
                 count: doc.length,
                 list: doc
@@ -45,7 +45,57 @@ module.exports = function () {
             let newToken = jwt.sign(verifyToken.username)
             res.json({
               status: 1,
-              msg: '更新token',
+              msg: '查询成功',
+              result: {
+                newToken: newToken,
+                count: doc.length,
+                list: doc
+              }
+            })
+          }
+        }
+      })
+    }
+  })
+
+  // 修改商品
+  router.post('/modify', (req, res, next) =>{
+    let verifyToken = jwt.verify(req.body.token)
+    if (verifyToken === 'invalid') {
+      res.json({
+        status: -1,
+        msg: '登录超时'
+      })
+    } else {
+      goods().update({
+        _id: req.body._id
+      }, {
+        productName: req.body.productName,
+        purchasePrice: req.body.purchasePrice,
+        productPrice: req.body.productPrice,
+        productCountry: req.body.productCountry,
+        productClass: req.body.productClass
+      }, (err, doc) => {
+        if (err) {
+          res.json({
+            status: -1,
+            msg: err.message
+          })
+        } else {
+          // token剩余时间
+          let remainTime = verifyToken.exp - Math.round(new Date() / 1000)
+          // 如果token剩余时间大于15分钟，则不更新token
+          if (remainTime > 900) {
+            res.json({
+              status: 0,
+              msg: '修改成功',
+            })
+          // 否则更新token
+          } else {
+            let newToken = jwt.sign(verifyToken.username)
+            res.json({
+              status: 1,
+              msg: '修改成功',
               result: {
                 newToken: newToken,
                 count: doc.length,
