ba25c9939f6f4037e67a6ae550dac29ef19317b4
孙昊翔 <673686754@qq.com>
2018-01-19 15:34:12 +0800

    代码优化

--- routes/login/index.js
+++ routes/login/index.js
@@ -3,37 +3,25 @@ const router = express.Router()
 const users = require('../../models/users')
 const jwt = require('../../jwt/jwt')
 const md5 = require('../../md5/md5')
+const { statusHandler } = require('../../lib/statusHandler')
 
 module.exports = function () {
   router.post('/', (req, res, next) => {
     users().find({username: req.body.username}, (err, doc) => {
       if (err) {
-        res.json({
-          status: -1,
-          msg: err.message
-        })
+        statusHandler(res, -1, err.message)
       } else {
         if (doc.length === 0) {
-          res.json({
-            status: -1,
-            msg: '账号不存在'
-          })
+          statusHandler(res, -1, '账号不存在')
         } else {
           let password = md5(req.body.password)
           if (password !== doc[0].password) {
-            res.json({
-              status: -1,
-              msg: '密码错误'
-            })
+            statusHandler(res, -1, '密码错误')
           } else {
             let signToken = jwt.sign(req.body.username)
-            res.json({
-              status: 0,
-              msg: '登陆成功',
-              result: {
-                username: doc[0].username,
-                token: signToken
-              }
+            statusHandler(res, 0, '登陆成功', {
+              username: doc[0].username,
+              token: signToken
             })
           }
         }
--- routes/register/index.js
+++ routes/register/index.js
@@ -2,28 +2,24 @@ const express = require('express')
 const router = express.Router()
 const users = require('../../models/users')
 const md5 = require('../../md5/md5')
+const { statusHandler } = require('../../lib/statusHandler')
 
 module.exports = function () {
   router.post('/', (req, res, next) => {
     users().find({username: req.body.username}, (err, doc) => {
       if (err) {
-        res.json({
-          status: -1,
-          msg: err.message
-        })
+        statusHandler(res, -1, err.message)
       } else {
         if (doc.length !== 0) {
-          res.json({
-            status: -1,
-            msg: '账号已存在'
-          })
+          statusHandler(res, -1, '账号已存在')
         } else {
           let password = md5(req.body.password)
           users().create({username: req.body.username, password: password}, (err) => {
-            res.json({
-              status: 0,
-              msg: '注册成功'
-            })
+            if (err) {
+              statusHandler(res, -1, err.message)
+            } else {
+              statusHandler(res, 0, '注册成功')
+            }
           })
         }
       }
