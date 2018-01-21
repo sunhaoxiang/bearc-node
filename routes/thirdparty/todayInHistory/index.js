fea54af0c3322e62a23086018e394cc0962d042d
孙昊翔 <673686754@qq.com>
2018-01-21 16:54:52 +0800

    历史上的今天详情接口

--- routes/thirdparty/todayInHistory/index.js
+++ routes/thirdparty/todayInHistory/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const random = require('./random')
+const detail = require('./detail')
 
 module.exports = function () {
-  // 列表
+  // 随机获得历史上的今天数据
   router.use('/random', random())
+
+  // 获得某条历史上的今天详细数据
+  router.use('/detail', detail())
 
   return router
 }
