239fb0addc5a6eb06882faa949fbc56a817c3b5c
孙昊翔 <673686754@qq.com>
2018-02-03 10:44:33 +0800

    注册添加邀请码

--- routes/register/index.js
+++ routes/register/index.js
@@ -2,28 +2,33 @@ const express = require('express')
 const router = express.Router()
 const users = require('../../models/users')
 const md5 = require('../../md5/md5')
+const { invitationCode } = require('../../config/config')
 const { statusHandler } = require('../../lib/statusHandler')
 
 module.exports = function () {
   router.post('/', (req, res, next) => {
-    users().find({username: req.body.username}, (err, doc) => {
-      if (err) {
-        statusHandler(res, 500, err.message)
-      } else {
-        if (doc.length !== 0) {
-          statusHandler(res, -1, '账号已存在')
+    if (req.body.invitationCode === invitationCode) {
+      users().find({username: req.body.username}, (err, doc) => {
+        if (err) {
+          statusHandler(res, 500, err.message)
         } else {
-          let password = md5(req.body.password)
-          users().create({username: req.body.username, password: password}, (err) => {
-            if (err) {
-              statusHandler(res, 500, err.message)
-            } else {
-              statusHandler(res, 0, '注册成功')
-            }
-          })
+          if (doc.length !== 0) {
+            statusHandler(res, -1, '账号已存在')
+          } else {
+            let password = md5(req.body.password)
+            users().create({username: req.body.username, password: password}, (err) => {
+              if (err) {
+                statusHandler(res, 500, err.message)
+              } else {
+                statusHandler(res, 0, '注册成功')
+              }
+            })
+          }
         }
-      }
-    })
+      })
+    } else {
+      statusHandler(res, -1, '邀请码不正确')
+    }
   })
 
   return router
